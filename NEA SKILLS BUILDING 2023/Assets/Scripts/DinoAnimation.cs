cd96a49ef9aecb408bc5be1fae648b01992a4e7a
TempleJ01 <temple.j01@elliotthudsoncollege.ac.uk>
2023-06-08 13:17:20 +0100

    Slide 20
    Finish Coding Transitions

---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/DinoAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DinoAnimation : MonoBehaviour {
+
+	Animator MyAnimator;
+	// Use this for initialization
+	void Start () {
+		MyAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			MyAnimator.SetBool ("walk", true);
+		} else {
+			MyAnimator.SetBool ("walk", false);
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			MyAnimator.SetBool ("dead", true);
+		} else {
+			MyAnimator.SetBool ("dead", false);
+		}
+	}
+}
