eced2c1cbf92da2d1d9bb22f01d9e08d3439db9d
TempleJ01 <temple.j01@elliotthudsoncollege.ac.uk>
2023-06-13 10:58:21 +0100

    Slide 34
    Spike debugging not working - check next lesson

---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMovement : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 5.1f;
+		rb = GetComponent<Rigidbody2D> ();
+			
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (Input.GetAxis ("Horizontal") * Time.deltaTime * speed, 0, 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			rb.AddForce (Vector2.up * 300);
+		}
+	}
+}
