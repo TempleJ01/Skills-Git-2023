172f784d92658a21fa37afd84e50128b5001f06e
TempleJ01 <temple.j01@elliotthudsoncollege.ac.uk>
2023-06-26 16:19:24 +0100

    Coin Sounds Maybe Done
    In Hudson Hub could not check audio.

---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioClip coin;
+	private AudioSource source;
+
+
+	// Use this for initialization
+	void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("BING!");
+		source.PlayOneShot (coin, 1);
+	}
+}
