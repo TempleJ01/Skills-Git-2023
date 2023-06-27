a3e2650ed02634de9ac8b811d06dd73061db766a
TempleJ01 <temple.j01@elliotthudsoncollege.ac.uk>
2023-06-27 09:20:05 +0100

    End of Task 8
    All sounds working

--- NEA SKILLS BUILDING 2023/Assets/Scripts/SoundManager.cs
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/SoundManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour {
 
 	public AudioClip coin;
+	public AudioClip life;
+	public AudioClip bomb;
 	private AudioSource source;
 
 
@@ -21,5 +23,13 @@ public class SoundManager : MonoBehaviour {
 	private void OnTriggerEnter2D(Collider2D other) {
 		Debug.Log ("BING!");
 		source.PlayOneShot (coin, 1);
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "extraLife") {
+			source.PlayOneShot (life, 1);
+		} else if (other.gameObject.tag == "vulnerable") {
+			source.PlayOneShot (bomb, 1);
+		}
 	}
 }
