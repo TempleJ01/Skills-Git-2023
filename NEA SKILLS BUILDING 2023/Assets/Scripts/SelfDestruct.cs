9ac0946531fb8637574c0e798cb7df62615cad5c
TempleJ01 <temple.j01@elliotthudsoncollege.ac.uk>
2023-06-28 13:02:20 +0100

    End of Task 12
    Bomb go boom

---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, 10);
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2(Random.Range(-5f,5f), Random.Range(5f,10f)) , ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDestroy() {
+		Debug.Log ("A bomb was destroyed");
+	}
+}
---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/bombSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bombSpawner : MonoBehaviour {
+
+	public GameObject bomb;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawner", 2, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Spawner() {
+		Instantiate (bomb, new Vector3 (34, 10, 0), Quaternion.identity);
+	}
+}
