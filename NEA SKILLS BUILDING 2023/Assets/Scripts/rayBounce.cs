9b24e62a0ebc32da61e2141e242c96f45df90a8d
TempleJ01 <temple.j01@elliotthudsoncollege.ac.uk>
2023-06-28 13:21:17 +0100

    Working on Ray Casting
    Fancy!!

---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/rayBounce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+	Rigidbody2D bomb;
+
+	// Use this for initialization
+	void Start () {
+		bomb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+}
+
